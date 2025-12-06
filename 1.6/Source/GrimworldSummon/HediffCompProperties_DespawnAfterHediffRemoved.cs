0dd8e4ff0b756ad98149ffdcca8f36f3243c2c65
doop0707 <alma.hue75@gmail.com>
2025-12-06 22:20:46 +0700

    implemented summon ability for emperor helmet

---
+++ 1.6/Source/GrimworldSummon/HediffCompProperties_DespawnAfterHediffRemoved.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace GrimworldSummon
+{
+    public class HediffCompProperties_DespawnAfterHediffRemoved : HediffCompProperties
+    {
+        public HediffCompProperties_DespawnAfterHediffRemoved()
+        {
+            compClass = typeof(HediffComp_DespawnAfterHediffRemoved);
+        }
+    }
+}
